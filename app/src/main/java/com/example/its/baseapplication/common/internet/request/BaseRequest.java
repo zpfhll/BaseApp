3df6382e06f32e6db3a7a57fd219e2636d08ae92
赵鹏飞 <>
2020-06-25 17:06:44 +0800

    no message

--- app/src/main/java/com/example/its/baseapplication/common/internet/request/BaseRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.its.baseapplication.common.internet.request;
-
-
-
-public class BaseRequest {
-}
--- app/src/main/java/com/example/its/baseapplication/common/internet/response/BaseResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.its.baseapplication.common.internet.response;
-
-import java.io.Serializable;
-
-public class BaseResponse implements Serializable {
-
-}
